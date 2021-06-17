ad3b2fbf7aece56512ec40e29ee6672f264267b9
Stephan <sleuth56@protonmail.com>
2021-06-17 17:04:24 -0400

    Add !send functionality step 1

--- container/Skill/main.py
+++ container/Skill/main.py
@@ -1,6 +1,7 @@
 from opsdroid.connector.matrix import ConnectorMatrix
-from opsdroid.events import Message, OpsdroidStarted, UserInvite, JoinRoom
-from opsdroid.matchers import match_event, match_regex, match_always
+from opsdroid.events import Message, UserInvite, JoinRoom
+from opsdroid.constraints import constrain_connectors
+from opsdroid.matchers import match_event, match_regex
 from opsdroid.skill import Skill
 import logging
 
@@ -14,46 +15,66 @@ class ANSSkill(Skill):
 
   # Join all rooms on invite.
   @match_event(UserInvite)
-  async def on_invite_to_room(self, invite):
+  async def on_invite_to_room(self, config, invite):
     _LOGGER.info(f"Got room invite for {invite.target}.")
     if self.join_when_invited:
       _LOGGER.debug(f"Joining room from invite.")
       await invite.respond(JoinRoom())
+      if not config.get('room_notify') in invite.target:
+        await invite.respond("hi")
+        
+      
+
+  # @match_event(JoinRoom)
+  # async def on_join(self, join):
+  #   opsdroid.send(Message(text="Hi, I'm new", target=self.config.get('room_notify')))
+    
 
   @match_regex(r'(?P<string>.*)')
-  async def last_seen(self, opsdroid, config, message):
-    # Grabs the regex string. In this case it's the whole message.
-    string = message.regex.group('string')
+  @constrain_connectors(['matrix'])
+  async def process_message(self, opsdroid, config, Message):
+    string = Message.regex.group('string')
 
-    # Attempts to get the current message state of the room
-    content = await opsdroid.memory.get(message.raw_event["room_id"])
+    if ("!send" in string):
+      output = f'{Message.user}<br>'
+      messages = await opsdroid.memory.get(Message.raw_event["room_id"])
+      keys = list(messages.keys())
+      _LOGGER.info(keys)
+      for i in range(len(keys)):
+        output += f'{messages[keys[i]]}<br>'
+      await Message.respond(output)
+      await self.opsdroid.send(Message(text=output, target=self.config.get('room_notify')))
 
-    # If this returns none create a new dictionary
-    if (content == None):
-      content = {}
-
-    # Extra logging for debuging
-    if (self.logging == True):
-      _LOGGER.info(message.raw_event)
-      _LOGGER.info(type(content))
-
-    # Checks if this message was an edit
-    if ("m.relates_to" in message.raw_event['content']):
-      if (self.logging == True):
-        _LOGGER.info(str(message.raw_event['content']['m.new_content']['body']))
-
-      # Updates the original message with the edit 
-      content[message.raw_event['content']['m.relates_to']['event_id']] = message.raw_event['content']['m.new_content']['body']
     else:
+      # Attempts to get the current message state of the room
+      content = await opsdroid.memory.get(Message.raw_event["room_id"])
+
+      # If this returns none create a new dictionary
+      if (content == None):
+        content = {}
+
+      # Extra logging for debuging
       if (self.logging == True):
-        _LOGGER.info(await opsdroid.memory.get(message.raw_event["room_id"]))
+        _LOGGER.info(Message.raw_event)
+        _LOGGER.info(type(content))
 
-      # Creates new entry for the message
-      content[message.raw_event["event_id"]] = message.raw_event["content"]["body"]
+      # Checks if this message was an edit
+      if ("m.relates_to" in Message.raw_event['content']):
+        if (self.logging == True):
+          _LOGGER.info(str(Message.raw_event['content']['m.new_content']['body']))
 
-    # Log check from the edited dictionary
-    if (self.logging == True):
-      _LOGGER.info(str(content))
+        # Updates the original message with the edit 
+        content[Message.raw_event['content']['m.relates_to']['event_id']] = Message.raw_event['content']['m.new_content']['body']
+      else:
+        if (self.logging == True):
+          _LOGGER.info(await opsdroid.memory.get(Message.raw_event["room_id"]))
 
-    # Store the dictionary as the room id
-    await opsdroid.memory.put(message.raw_event["room_id"], content)
+        # Creates new entry for the message
+        content[Message.raw_event["event_id"]] = Message.raw_event["content"]["body"]
+
+      # Log check from the edited dictionary
+      if (self.logging == True):
+        _LOGGER.info(str(content))
+
+      # Store the dictionary as the room id
+      await opsdroid.memory.put(Message.raw_event["room_id"], content)
